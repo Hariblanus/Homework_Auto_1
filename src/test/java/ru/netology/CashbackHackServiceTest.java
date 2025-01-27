a5b3204b4419cce2aa60fcd07c09dc6f33d37e29
Artem Vozniak <voznyako@gmail.com>
2025-01-27 15:59:55 +0400

    Task 1

---
+++ src/test/java/ru/netology/CashbackHackServiceTest.java
@@ -0,0 +1,52 @@
+package ru.netology;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class CashbackHackServiceTest {
+    @Test
+    public void shouldReturn1000IfAmountIs0() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 0;
+        int expected = 1000;
+        int actual = service.remain(amount);
+        Assert.assertEquals(actual, expected);
+    }
+
+    @Test
+    public void shouldReturn500IfAmountIs500() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 500;
+        int expected = 500;
+        int actual = service.remain(amount);
+        Assert.assertEquals(actual, expected);
+    }
+
+    @Test
+    public void shouldReturn1IfAmountIs999() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 999;
+        int expected = 1;
+        int actual = service.remain(amount);
+        Assert.assertEquals(actual, expected);
+    }
+
+    @Test
+    public void shouldReturn1000IfAmountIs1000() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 1000;
+        int expected = 1000;
+        int actual = service.remain(amount);
+        Assert.assertEquals(actual, expected);
+    }
+
+    @Test
+    public void shouldReturn999IfAmountIs1() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 1;
+        int expected = 999;
+        int actual = service.remain(amount);
+        Assert.assertEquals(actual, expected);
+    }
+}
+
